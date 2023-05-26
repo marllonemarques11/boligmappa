400e1257b2b1300e6dbd874678a9ed249ee2b874
Marllone Marques <marllonemarques@outlook.com>
2023-05-26 02:07:11 +0100

    Camada de dados, e config do EF para o PostgreSQL

---
+++ src/DT.Infra/Repository/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace DT.Infra.Repository
+{
+    public class AppDbContext : DbContext
+    {
+        protected readonly IConfiguration _configuration;
+
+        public AppDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseNpgsql(_configuration.GetConnectionString("DummyDB"));
+        }
+    }
+}
