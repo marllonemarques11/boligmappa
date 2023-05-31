e8537fcd1cc5b4d8d3c4513f5580da97dbb539ee
Marllone Marques <marllonemarques@outlook.com>
2023-05-31 01:59:31 +0100

    Entry point layer

---
+++ src/API/DT.EntryPoints/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DT.BusinessRules.Contracts;
+using DT.BusinessRules.Rules;
+
+namespace DT.EntryPoints.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRules _userRules;
+
+        public UserController(UserRules userRules)
+        {
+            _userRules = userRules;
+        }
+
+        [HttpGet]
+        [Route("GetUsersTodos")]
+        public async Task<IActionResult> GetUsersTodos() {
+            try
+            {
+                return Ok(await _userRules.GetUsersTodos());
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+        [HttpGet]
+        [Route("GetUsersPosts")]
+        public async Task<IActionResult> GetUsersPosts() {
+            try
+            {
+                return Ok(await _userRules.GetUsersPosts());
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+            
+        }
+    }
+}
